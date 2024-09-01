22c3b65fb15e84ae4d9bbf00e25ac8e8257901c9
Prakhar Kumar <prakhar.meerut@gmail.com>
2024-09-01 23:14:38 +0530

    Thread builder and scoped threads
    Signed-off-by: Prakhar Kumar <prakhar.meerut@gmail.com>

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -14,6 +14,14 @@ fn do_math(i: u32) -> u32 {
     n
 }
 
+// a function which prints some info related to thread
+fn my_thread() {
+    println!(
+        "Hello from thread named {}",
+        std::thread::current().name().unwrap()
+    );
+}
+
 fn main() {
     let thread_handle = std::thread::spawn(hello_thread);
     // when main programs ends all child threads will also end so we need to wait for children
@@ -57,4 +65,54 @@ fn main() {
         .into_iter()
         // unwrap will give the return value
         .for_each(|handle| println!("{}", handle.join().unwrap()));
+
+    // Dividing Workloads into chunks and handling each chunk in a thread
+    let to_add: Vec<u32> = (0..5000).collect(); // 0-4999
+    let mut thread_handles: Vec<std::thread::JoinHandle<u32>> = Vec::new();
+    let chunks = to_add.chunks(8);
+
+    for chunk in chunks {
+        let my_chunk = chunk.to_owned();
+        thread_handles.push(std::thread::spawn(move || my_chunk.iter().sum()));
+    }
+
+    let mut sum = 0;
+    for handle in thread_handles {
+        sum += handle.join().unwrap();
+    }
+    println!("Sum is {}", sum);
+
+    // For more control and getting more info about thread you are running.
+    // Using thread builder to configure thread
+    // std::mem::size_of::<usize> gives size of uint on the machine
+    let thread_handle = std::thread::Builder::new()
+        .name("named_thread".to_string())
+        .stack_size(std::mem::size_of::<usize>() * 4) // memory size in bytes, default is 2 MiB on linux
+        .spawn(my_thread)
+        .unwrap();
+    thread_handle.join().unwrap();
+
+    // Sharing data using scoped threads - inspired by rayon library.
+    // automating joining and auto cleanup as within a scope.
+    // all threads terminate when  scope ends
+    let chunks = to_add.chunks(8);
+
+    // notice how we dont need to move when using scoped threads
+    // scoped threads handles ownership hence moving not needed.
+    let sum = std::thread::scope(|s| {
+        let mut thread_handles = Vec::new();
+
+        for chunk in chunks {
+            // no move here either.
+            let thread_handle = s.spawn(|| chunk.iter().sum::<u32>());
+            thread_handles.push(thread_handle);
+        }
+
+        thread_handles
+            .into_iter()
+            .map(|handle| handle.join().unwrap())
+            .sum::<u32>() // no semicolon hence value is returned
+    });
+
+    println!("Scoped Thread Sum is {}", sum);
 }
